9fb956d598def9c9f5bbe6fb0ce2c73d0d07676c
Camille Delezinier <camille.delezinier@etu.univ-poitiers.fr>
2025-09-24 18:20:53 +0200

    Création NotFoundPages

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import AboutPage from './pages/AboutPage';
 import CharactersPage from './pages/CharactersPage';
 import ContactPage from './pages/ContactPage';
-import Layout from './layout';
+import Layout from './Layout';
+import NotFoundPage from './pages/NotFoundPage';
 
 // routes of the application
 const routes = [
@@ -24,6 +25,11 @@ const routes = [
         path: "/contact",
         Component: ContactPage
       },
+      {
+        // 404 page
+        path: "*",
+        Component: NotFoundPage
+      }
     ],
   },
 ]
